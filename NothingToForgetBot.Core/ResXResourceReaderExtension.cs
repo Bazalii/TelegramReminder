d79a7e6d0ada160cb9e952095e1901bad3b45901
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-06 23:02:32 +0300

    feat: realise GetString extension method for ResXResourceReader

---
+++ NothingToForgetBot.Core/ResXResourceReaderExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Resources.NetStandard;
+
+namespace NothingToForgetBot.Core;
+
+public static class ResXResourceReaderExtension
+{
+    public static string? GetString(this ResXResourceReader resourceReader, string key)
+    {
+        var dictionary = resourceReader.GetEnumerator();
+
+        while (dictionary.MoveNext())
+        {
+            var currentKey = dictionary.Key as string ??
+                             throw new DataException("Not correct data in resource keys");
+
+            if (currentKey == key)
+            {
+                return dictionary.Value as string ?? dictionary.Value!.ToString();
+            }
+        }
+
+        return null;
+    }
+}
