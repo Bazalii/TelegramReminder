7788a013174ec83972642e9cab4f8c422ab21b68
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-06 22:59:23 +0300

    feat: realise CommandParser and add corresponding interface

---
+++ NothingToForgetBot.Core/Commands/Parsers/ICommandParser.cs
@@ -0,0 +1,8 @@
+﻿using NothingToForgetBot.Core.Enums;
+
+namespace NothingToForgetBot.Core.Commands.Parsers;
+
+public interface ICommandParser
+{
+    public Command Parse(string message);
+}
---
+++ NothingToForgetBot.Core/Commands/Parsers/Implementations/CommandParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources.NetStandard;
+using NothingToForgetBot.Core.Enums;
+
+namespace NothingToForgetBot.Core.Commands.Parsers.Implementations;
+
+public class CommandParser : ICommandParser
+{
+    private readonly string _resourcesFile;
+
+    public CommandParser(string resourcesFile)
+    {
+        _resourcesFile = resourcesFile;
+    }
+
+    public Command Parse(string message)
+    {
+        var resourceReader = new ResXResourceReader(_resourcesFile);
+
+        if (message == resourceReader.GetString("CommandGuide"))
+        {
+            return Command.Guide;
+        }
+
+        if (message == resourceReader.GetString("CommandLanguage"))
+        {
+            return Command.Language;
+        }
+
+        if (message == resourceReader.GetString("CommandList"))
+        {
+            return Command.List;
+        }
+
+        return Command.NotCommand;
+    }
+}
