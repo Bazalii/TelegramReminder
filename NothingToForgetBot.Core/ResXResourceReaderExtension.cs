c963dc5605d4f7f603e6a68f0deb08b345ae5a28
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-07 18:48:39 +0300

    feat: improve GetString in ResXResourceReaderExtension
    - Change return type to string
    - Add usage of new exceptions

--- NothingToForgetBot.Core/ResXResourceReaderExtension.cs
+++ NothingToForgetBot.Core/ResXResourceReaderExtension.cs
@@ -1,11 +1,12 @@
 ﻿using System.Data;
 using System.Resources.NetStandard;
+using NothingToForgetBot.Core.Exceptions;
 
 namespace NothingToForgetBot.Core;
 
 public static class ResXResourceReaderExtension
 {
-    public static string? GetString(this ResXResourceReader resourceReader, string key)
+    public static string GetString(this ResXResourceReader resourceReader, string key)
     {
         var dictionary = resourceReader.GetEnumerator();
 
@@ -16,10 +17,11 @@ public static class ResXResourceReaderExtension
 
             if (currentKey == key)
             {
-                return dictionary.Value as string ?? dictionary.Value!.ToString();
+                return dictionary.Value as string ??
+                       throw new NullResourceValueException("Resource value cannot be null!");
             }
         }
 
-        return null;
+        throw new ResourceValueNotFoundException($"Resource with key:{key} is not found!");
     }
 }
