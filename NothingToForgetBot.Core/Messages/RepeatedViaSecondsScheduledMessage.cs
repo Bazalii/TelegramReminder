ec30351bfc73bd13c48863059205bf93fe263b66
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-15 19:13:47 +0300

    feat: realise RepeatedViaSecondsScheduledMessage

---
+++ NothingToForgetBot.Core/Messages/RepeatedViaSecondsScheduledMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace NothingToForgetBot.Core.Messages;
+
+public class RepeatedViaSecondsScheduledMessage : Message
+{
+    public int Interval { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
