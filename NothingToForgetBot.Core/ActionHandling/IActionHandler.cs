ea7211edebaca71cd78d774ca90ca6aa2fd16140
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-15 19:51:36 +0300

    feat: realise interface IActionHandler

---
+++ NothingToForgetBot.Core/ActionHandling/IActionHandler.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NothingToForgetBot.Core.ActionHandling;
+
+public interface IMessageHandler
+{
+    Task HandleChatUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken);
+
+    Task HandlePollingError(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken);
+}
