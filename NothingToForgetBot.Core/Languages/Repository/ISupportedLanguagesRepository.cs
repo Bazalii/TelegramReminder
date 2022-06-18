3f4eeb5d1f1352ce5ab04a67e9545f53e66448d9
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-18 18:00:45 +0300

    feat: assign interface for SupportedLanguageRepository

---
+++ NothingToForgetBot.Core/Languages/Repository/ISupportedLanguagesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace NothingToForgetBot.Core.Languages.Repository;
+
+public interface ISupportedLanguagesRepository
+{
+    Task Add(string language);
+    
+    Task<List<string>> GetAll();
+
+    Task Remove(string language);
+}
