290d040e56c685be16bfcff707e054e0197bf1af
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-19 13:46:13 +0300

    feat: assign interface for ScheduledMessageRepository

---
+++ NothingToForgetBot.Core/Messages/Repositories/IScheduledMessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using NothingToForgetBot.Core.Messages.Models;
+
+namespace NothingToForgetBot.Core.Messages.Repositories;
+
+public interface IScheduledMessageRepository
+{
+    Task Add(ScheduledMessage scheduledMessage);
+
+    Task<ScheduledMessage> GetById(Guid id);
+    
+    Task<List<ScheduledMessage>> GetAllByChatId(long chatId);
+    
+    Task Update(ScheduledMessage scheduledMessage);
+    
+    Task Remove(Guid id);
+}
