797cb2147b1403da7f287a32e98bc1b5b185c17f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-19 12:43:06 +0300

    feat: add interface for NoteRepository

---
+++ NothingToForgetBot.Core/Notes/Repositories/INoteRepository.cs
@@ -0,0 +1,16 @@
+﻿using NothingToForgetBot.Core.Notes.Models;
+
+namespace NothingToForgetBot.Core.Notes.Repositories;
+
+public interface INoteRepository
+{
+    Task Add(Note note);
+
+    Task<Note> GetById(Guid id);
+    
+    Task<List<Note>> GetAllByChatId(long chatId);
+    
+    Task Update(Note note);
+    
+    Task Remove(Guid id);
+}
