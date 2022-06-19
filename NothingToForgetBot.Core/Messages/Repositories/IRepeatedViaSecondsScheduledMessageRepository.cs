2eeeb20bfbb711d76edaf8820e80d77be2133dab
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-19 13:46:51 +0300

    feat: add interface for RepeatedViaSecondsScheduledMessageRepository

---
+++ NothingToForgetBot.Core/Messages/Repositories/IRepeatedViaSecondsScheduledMessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using NothingToForgetBot.Core.Messages.Models;
+
+namespace NothingToForgetBot.Core.Messages.Repositories;
+
+public interface IRepeatedViaSecondsScheduledMessageRepository
+{
+    Task Add(RepeatedViaSecondsScheduledMessage scheduledMessage);
+
+    Task<RepeatedViaSecondsScheduledMessage> GetById(Guid id);
+    
+    Task<List<RepeatedViaSecondsScheduledMessage>> GetAllByChatId(long chatId);
+    
+    Task Update(RepeatedViaSecondsScheduledMessage scheduledMessage);
+    
+    Task Remove(Guid id);
+}
