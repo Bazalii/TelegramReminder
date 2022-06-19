f9cde988c8777c86249535b46ac83bb67e0b31bb
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-19 12:43:57 +0300

    feat: realise NoteHandler

--- NothingToForgetBot.Core/Notes/Handlers/Implementations/NoteHandler.cs
+++ NothingToForgetBot.Core/Notes/Handlers/Implementations/NoteHandler.cs
@@ -1,11 +1,19 @@
 ﻿using NothingToForgetBot.Core.Notes.Models;
+using NothingToForgetBot.Core.Notes.Repositories;
 
 namespace NothingToForgetBot.Core.Notes.Handlers.Implementations;
 
 public class NoteHandler : INoteHandler
 {
+    private readonly INoteRepository _noteRepository;
+
+    public NoteHandler(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+    
     public Task Handle(Note note)
     {
-        throw new NotImplementedException();
+        return _noteRepository.Add(note);
     }
 }
