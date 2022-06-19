c9e8c57543d80642a658396683ec443c7cf33ba6
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-19 13:49:50 +0300

    feat: assign interface for ChatLanguageRepository

---
+++ NothingToForgetBot.Core/Languages/Repository/IChatLanguageRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace NothingToForgetBot.Core.Languages.Repository;
+
+public interface IChatLanguageRepository
+{
+    Task Add(ChatWithLanguage chatWithLanguage);
+
+    Task<string> GetLanguageByChatId(long chatId);
+    
+    Task Update(ChatWithLanguage chatWithLanguage);
+    
+    Task RemoveByChatId(long chatId);
+}
