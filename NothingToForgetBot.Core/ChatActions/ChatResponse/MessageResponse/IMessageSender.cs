334f0741ea5d5153b308e1f9d4aeee3490202f0a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-24 11:03:12 +0300

    feat: assign interface for MessageSender

---
+++ NothingToForgetBot.Core/ChatActions/ChatResponse/MessageResponse/IMessageSender.cs
@@ -0,0 +1,6 @@
+﻿namespace NothingToForgetBot.Core.ChatActions.ChatResponse.MessageResponse;
+
+public interface IMessageSender
+{
+    public Task SendMessageToChat(long chatId, string messageText, CancellationToken cancellationToken);
+}
