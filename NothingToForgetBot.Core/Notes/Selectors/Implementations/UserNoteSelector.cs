be1ff1b2d61596d298ae5f2ef8b1bd692e5e0896
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-29 20:51:01 +0300

    feat: realise UserNoteSelector

---
+++ NothingToForgetBot.Core/Notes/Selectors/Implementations/UserNoteSelector.cs
@@ -0,0 +1,19 @@
+﻿using NothingToForgetBot.Core.Notes.Models;
+using NothingToForgetBot.Core.Notes.Repositories;
+
+namespace NothingToForgetBot.Core.Notes.Selectors.Implementations;
+
+public class UserNoteSelector : IUserNoteSelector
+{
+    private readonly INoteRepository _noteRepository;
+
+    public UserNoteSelector(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+
+    public Task<List<Note>> Select(long chatId, CancellationToken cancellationToken)
+    {
+        return _noteRepository.GetAllByChatId(chatId, cancellationToken);
+    }
+}
