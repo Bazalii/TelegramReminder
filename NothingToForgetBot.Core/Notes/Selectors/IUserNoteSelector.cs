3727dcf29b82f21ebf65de5db5e31f59f6d20fb4
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-29 20:50:43 +0300

    feat: assign interface for UserNoteSelector

---
+++ NothingToForgetBot.Core/Notes/Selectors/IUserNoteSelector.cs
@@ -0,0 +1,8 @@
+﻿using NothingToForgetBot.Core.Notes.Models;
+
+namespace NothingToForgetBot.Core.Notes.Selectors;
+
+public interface IUserNoteSelector
+{
+    Task<List<Note>> Select(long chatId, CancellationToken cancellationToken);
+}
