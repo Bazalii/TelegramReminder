c904755f6fe2287e9ab98718192ae829723ca957
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-30 20:05:03 +0300

    feat: realise ToString method in RepeatedViaSecondsScheduledMessage

--- NothingToForgetBot.Core/Messages/Models/RepeatedViaSecondsScheduledMessage.cs
+++ NothingToForgetBot.Core/Messages/Models/RepeatedViaSecondsScheduledMessage.cs
@@ -1,5 +1,12 @@
-﻿namespace NothingToForgetBot.Core.Messages.Models;
+﻿using System.Globalization;
+
+namespace NothingToForgetBot.Core.Messages.Models;
 
 public class RepeatedViaSecondsScheduledMessage : RepeatedMessage
 {
+    public string ToString(string every, string seconds, string until)
+    {
+        return
+            $"{Content} {every} {Interval} {seconds} {until} {EndDate.ToString(CultureInfo.GetCultureInfo("ru-RU"))}";
+    }
 }
