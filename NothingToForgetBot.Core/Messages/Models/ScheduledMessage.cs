b6202fd8cfb62d24afbfca46d2e652dcb49d918b
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-30 20:05:17 +0300

    feat: realise ToString method in ScheduledMessage

--- NothingToForgetBot.Core/Messages/Models/ScheduledMessage.cs
+++ NothingToForgetBot.Core/Messages/Models/ScheduledMessage.cs
@@ -1,6 +1,13 @@
-﻿namespace NothingToForgetBot.Core.Messages.Models;
+﻿using System.Globalization;
+
+namespace NothingToForgetBot.Core.Messages.Models;
 
 public class ScheduledMessage : Message
 {
     public DateTime PublishingDate { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Content} {PublishingDate.ToString(CultureInfo.GetCultureInfo("ru-RU"))}";
+    }
 }
