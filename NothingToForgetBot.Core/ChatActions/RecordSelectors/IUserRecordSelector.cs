fb5e59ebbbbaeb692d1bc680f9f75b6472aa64e0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-30 11:56:08 +0300

    feat: assign interface for UserRecordSelector

---
+++ NothingToForgetBot.Core/ChatActions/RecordSelectors/IUserRecordSelector.cs
@@ -0,0 +1,8 @@
+﻿using NothingToForgetBot.Core.ChatActions.RecordSelectors.Models;
+
+namespace NothingToForgetBot.Core.ChatActions.RecordSelectors;
+
+public interface IUserRecordSelector
+{
+    Task<UserRecords> Select(long chatId, CancellationToken cancellationToken);
+}
