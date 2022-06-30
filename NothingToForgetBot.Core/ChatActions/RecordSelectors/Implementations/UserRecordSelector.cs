8927aa43f20a2d1ec0c99dd1482727f4418827c1
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-06-30 11:56:25 +0300

    feat: realise UserRecordSelector

---
+++ NothingToForgetBot.Core/ChatActions/RecordSelectors/Implementations/UserRecordSelector.cs
@@ -0,0 +1,34 @@
+﻿using NothingToForgetBot.Core.ChatActions.RecordSelectors.Models;
+using NothingToForgetBot.Core.Messages.Selectors;
+using NothingToForgetBot.Core.Notes.Selectors;
+
+namespace NothingToForgetBot.Core.ChatActions.RecordSelectors.Implementations;
+
+public class UserRecordSelector : IUserRecordSelector
+{
+    private readonly IUserMessageSelector _messageSelector;
+
+    private readonly IUserNoteSelector _noteSelector;
+
+
+    public UserRecordSelector(IUserMessageSelector messageSelector, IUserNoteSelector noteSelector)
+    {
+        _messageSelector = messageSelector;
+        _noteSelector = noteSelector;
+    }
+
+    public async Task<UserRecords> Select(long chatId, CancellationToken cancellationToken)
+    {
+        var userMessages = await _messageSelector.Select(chatId, cancellationToken);
+
+        var userNotes = await _noteSelector.Select(chatId, cancellationToken);
+
+        return new UserRecords
+        {
+            ScheduledMessages = userMessages.ScheduledMessages,
+            RepeatedViaMinutesScheduledMessages = userMessages.RepeatedViaMinutesScheduledMessages,
+            RepeatedViaSecondsScheduledMessages = userMessages.RepeatedViaSecondsScheduledMessages,
+            Notes = userNotes
+        };
+    }
+}
