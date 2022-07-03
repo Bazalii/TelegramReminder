dbc8760b7e5022a44c69b2a08afa539590024aee
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-03 23:11:17 +0300

    feat: realise CommandWithArgumentsParser

---
+++ NothingToForgetBot.Core/Commands/Parsers/Implementations/CommandWithArgumentsParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Resources.NetStandard;
+using System.Text.RegularExpressions;
+using NothingToForgetBot.Core.Enums;
+
+namespace NothingToForgetBot.Core.Commands.Parsers.Implementations;
+
+public class CommandWithArgumentsParser : ICommandWithArgumentsParser
+{
+    private readonly ResXResourceReader _resourceReader;
+
+    public CommandWithArgumentsParser(ResXResourceReader resourceReader)
+    {
+        _resourceReader = resourceReader;
+    }
+
+    public CommandWithArguments Parse(string message)
+    {
+        var deleteCommandResourceValue = _resourceReader.GetString("CommandDelete");
+        
+        var deleteCommandRegex = new Regex($"{deleteCommandResourceValue}\\s\\d");
+
+        if (deleteCommandRegex.IsMatch(message))
+        {
+            return CommandWithArguments.Delete;
+        }
+
+        return CommandWithArguments.NotCommandWithArguments;
+    }
+}
