b786c80d6a2e67181441f5283610a10fce778b4e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-05 23:38:58 +0300

    feat: assign interface for CommandWithArgumentsHandler

---
+++ NothingToForgetBot.Core/Commands/Handlers/ICommandWithArgumentsHandler.cs
@@ -0,0 +1,8 @@
+﻿using NothingToForgetBot.Core.Enums;
+
+namespace NothingToForgetBot.Core.Commands.Handlers;
+
+public interface ICommandWithArgumentsHandler
+{
+    Task Handle(CommandWithArguments command, long chatId, string arguments, CancellationToken cancellationToken);
+}
