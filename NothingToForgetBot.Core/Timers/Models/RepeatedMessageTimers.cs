d05078aa5d14d2a2ab4dc7c800c353e29c5194ec
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-06 23:16:36 +0300

    feat: realise RepeatedMessageTimers model

---
+++ NothingToForgetBot.Core/Timers/Models/RepeatedMessageTimers.cs
@@ -0,0 +1,10 @@
+﻿namespace NothingToForgetBot.Core.Timers.Models;
+using Timer = System.Timers.Timer;
+
+
+public class RepeatedMessageTimers
+{
+    public Timer RepeatedTimer { get; set; }
+
+    public Timer EndTimer { get; set; }
+}
