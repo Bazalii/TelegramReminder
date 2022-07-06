c2a87434e87971977cd2993ce5ee3e78ebbc32e0
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-06 23:16:06 +0300

    feat: assign interface for TimerHandler

---
+++ NothingToForgetBot.Core/Timers/ITimerHandler.cs
@@ -0,0 +1,15 @@
+﻿using NothingToForgetBot.Core.Timers.Models;
+using Timer = System.Timers.Timer;
+
+namespace NothingToForgetBot.Core.Timers;
+
+public interface ITimerHandler
+{
+    Task Add(Guid id, Timer timer);
+    
+    Task Add(Guid id, RepeatedMessageTimers timers);
+    
+    Task RemoveRepeatedViaMinutesMessageTimers(Guid id, CancellationToken cancellationToken);
+    
+    Task RemoveRepeatedViaSecondsMessageTimers(Guid id, CancellationToken cancellationToken);
+}
