a14b1021cb9f14b1bcec2a88804f30952611c007
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-07 12:33:23 +0300

    fix: add RemoveScheduledMessageTimer to ITimerHandler

--- NothingToForgetBot.Core/Timers/ITimerHandler.cs
+++ NothingToForgetBot.Core/Timers/ITimerHandler.cs
@@ -8,6 +8,8 @@ public interface ITimerHandler
     Task Add(Guid id, Timer timer);
     
     Task Add(Guid id, RepeatedMessageTimers timers);
+
+    Task RemoveScheduledMessageTimer(Guid id, CancellationToken cancellationToken);
     
     Task RemoveRepeatedViaMinutesMessageTimers(Guid id, CancellationToken cancellationToken);
     
