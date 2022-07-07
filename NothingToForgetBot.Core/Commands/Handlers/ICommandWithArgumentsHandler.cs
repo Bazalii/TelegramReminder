c52ff36e9cd236678540cb512e6866516a516567
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-07 17:54:09 +0300

    feat: realise CommandWithArgumentsHandler

--- NothingToForgetBot.Core/Commands/Handlers/ICommandWithArgumentsHandler.cs
+++ NothingToForgetBot.Core/Commands/Handlers/ICommandWithArgumentsHandler.cs
@@ -4,5 +4,6 @@ namespace NothingToForgetBot.Core.Commands.Handlers;
 
 public interface ICommandWithArgumentsHandler
 {
-    Task Handle(CommandWithArguments command, long chatId, string arguments, CancellationToken cancellationToken);
+    Task Handle(CommandWithArguments command, long chatId, string localisation, string message,
+        CancellationToken cancellationToken);
 }
