bc98b6b1f53640bf8141451b86051b3a5ae330fd
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-11 23:21:32 +0300

    feat: realise MessageDbModel abstract class

---
+++ NothingToForgetBot.Data/Messages/Models/MessageDbModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NothingToForgetBot.Data.Messages.Models;
+
+public abstract class MessageDbModel
+{
+    public Guid Id { get; set; }
+
+    public long ChatId { get; set; }
+
+    public string Content { get; set; }
+}
