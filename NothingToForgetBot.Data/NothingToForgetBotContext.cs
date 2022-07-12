b053345cf0aa397ab61b47ce8b8486aba58f3264
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-12 18:38:39 +0300

    feat: realise NothingToForgetBotContext
    - Add all needed DbSets
    - Add OnConfiguring method
    - Add Factory class for DbContext creation

---
+++ NothingToForgetBot.Data/NothingToForgetBotContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using NothingToForgetBot.Data.Languages.Models;
+using NothingToForgetBot.Data.Messages.Models;
+using NothingToForgetBot.Data.Notes.Models;
+
+namespace NothingToForgetBot.Data;
+
+public class NothingToForgetBotContext : DbContext
+{
+    public DbSet<ChatWithLanguageDbModel> ChatsWithLanguage { get; set; }
+    
+    public DbSet<ScheduledMessageDbModel> ScheduledMessages { get; set; }
+    
+    public DbSet<RepeatedViaMinutesMessageDbModel> RepeatedViaMinutesMessages { get; set; }
+    
+    public DbSet<RepeatedViaSecondsMessageDbModel> RepeatedViaSecondsMessages { get; set; }
+    
+    public DbSet<NoteDbModel> Notes { get; set; }
+
+    public NothingToForgetBotContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(NothingToForgetBotContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSnakeCaseNamingConvention()
+            .UseLazyLoadingProxies();
+    }
+    
+    public class Factory : IDesignTimeDbContextFactory<NothingToForgetBotContext>
+    {
+        public NothingToForgetBotContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql("FakeConnectionStringOnlyForMigrations")
+                .Options;
+
+            return new NothingToForgetBotContext(options);
+        }
+    }
+}
