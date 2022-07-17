e873b1e119b190db9cc13f9197302b49b96458dc
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-17 23:16:44 +0300

    feat: realise AddData extension method for IServiceCollection

---
+++ NothingToForgetBot.Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NothingToForgetBot.Core;
+using NothingToForgetBot.Core.Languages.Repository;
+using NothingToForgetBot.Core.Messages.Repositories;
+using NothingToForgetBot.Core.Notes.Repositories;
+using NothingToForgetBot.Data.Languages.Repositories;
+using NothingToForgetBot.Data.Messages.Repositories;
+using NothingToForgetBot.Data.Notes.Repositories;
+
+namespace NothingToForgetBot.Data;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddSingleton<IChatLanguageRepository, ChatLanguageRepository>()
+            .AddSingleton<ISupportedLanguagesRepository, SupportedLanguagesRepository>()
+            .AddSingleton<IScheduledMessageRepository, ScheduledMessageRepository>()
+            .AddSingleton<IRepeatedViaMinutesScheduledMessageRepository, RepeatedViaMinutesMessageRepository>()
+            .AddSingleton<IRepeatedViaSecondsScheduledMessageRepository, RepeatedViaSecondsMessageRepository>()
+            .AddSingleton<INoteRepository, NoteRepository>()
+            .AddSingleton<IUnitOfWork, EfUnitOfWork>()
+            .AddDbContext<NothingToForgetBotContext>(options => options
+                .UseLazyLoadingProxies()
+                .UseNpgsql(configuration["DbConnectionString"]));
+
+        return services;
+    }
+}
