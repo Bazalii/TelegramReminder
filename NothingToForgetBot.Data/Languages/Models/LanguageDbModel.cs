86daf701f6ffceff4e1dec001538f709bad400fb
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-18 19:43:54 +0300

    feat: realise LanguageDbModel for saving supported languages

---
+++ NothingToForgetBot.Data/Languages/Models/LanguageDbModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NothingToForgetBot.Data.Languages.Models;
+
+public class LanguageDbModel
+{
+    public string Name { get; set; }
+    
+    internal class Map : IEntityTypeConfiguration<LanguageDbModel>
+    {
+        public void Configure(EntityTypeBuilder<LanguageDbModel> builder)
+        {
+            builder.ToTable("supported_languages");
+            builder.HasNoKey();
+        }
+    }
+}
