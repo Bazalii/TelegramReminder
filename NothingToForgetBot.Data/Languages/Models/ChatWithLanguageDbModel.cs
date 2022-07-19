b81b6c136f3b1a67088d37370a020328750d3290
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-07-19 12:42:23 +0300

    feat: add db entity configuration to ChatWithLanguageDbModel

--- NothingToForgetBot.Data/Languages/Models/ChatWithLanguageDbModel.cs
+++ NothingToForgetBot.Data/Languages/Models/ChatWithLanguageDbModel.cs
@@ -1,8 +1,20 @@
-﻿namespace NothingToForgetBot.Data.Languages.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NothingToForgetBot.Data.Languages.Models;
 
 public class ChatWithLanguageDbModel
 {
     public long ChatId { get; set; }
 
     public string Language { get; set; }
+    
+    internal class Map : IEntityTypeConfiguration<ChatWithLanguageDbModel>
+    {
+        public void Configure(EntityTypeBuilder<ChatWithLanguageDbModel> builder)
+        {
+            builder.ToTable("chats_with_language");
+            builder.HasKey(dbModel => dbModel.ChatId).HasName("pk_chat_with_language");
+        }
+    }
 }
