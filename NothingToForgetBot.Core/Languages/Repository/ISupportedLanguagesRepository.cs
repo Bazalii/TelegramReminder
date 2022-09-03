fbad77de5f22c1cf8779e3c9dc9121eab1a757fe
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-09-03 11:59:19 +0300

    refactor: remove blank lines between methods in interfaces

--- NothingToForgetBot.Core/Languages/Repository/ISupportedLanguagesRepository.cs
+++ NothingToForgetBot.Core/Languages/Repository/ISupportedLanguagesRepository.cs
@@ -3,8 +3,6 @@
 public interface ISupportedLanguagesRepository
 {
     Task Add(string languageName, CancellationToken cancellationToken);
-
     Task<List<string>> GetAll(CancellationToken cancellationToken);
-
     Task Remove(string languageName, CancellationToken cancellationToken);
 }
--- NothingToForgetBot.Core/Notes/Repositories/INoteRepository.cs
+++ NothingToForgetBot.Core/Notes/Repositories/INoteRepository.cs
@@ -5,12 +5,8 @@ namespace NothingToForgetBot.Core.Notes.Repositories;
 public interface INoteRepository
 {
     Task Add(Note note, CancellationToken cancellationToken);
-
     Task<Note> GetById(Guid id, CancellationToken cancellationToken);
-
     Task<List<Note>> GetAllByChatId(long chatId, CancellationToken cancellationToken);
-
     Task Update(Note note, CancellationToken cancellationToken);
-
     Task Remove(Guid id, CancellationToken cancellationToken);
 }
